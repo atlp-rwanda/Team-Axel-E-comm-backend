7cd091ca9dc6c23b9de17e1eb759e758e7e29af5
Clarisse ISINGIZWE <93644256+ISINGIZWEcla@users.noreply.github.com>
2023-02-01 21:45:28 +0200

    ft: Unit testing setUp

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,3 +1,20 @@
-/*
- *Example of a test file 🧪
- */
+import request from "supertest";
+import app from "./../src/app";
+
+ describe("Simple test case", () => {
+  it("Add two numbers", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+describe("Route test", () => {
+  it("get any specified route", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({ 
+    status: 200,
+    success: true,
+    message: `Welcome to team Axel's API! Endpoints available at http://localhost:8080/api/v1 + whatever endpoint you want to hit`,
+    });
+  });
+});
