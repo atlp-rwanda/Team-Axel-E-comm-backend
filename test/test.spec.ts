758c8636293b40b9d9820d7b042c2c26d973d0ca
muslim uwitondanishema <muslimuwitondanishema@gmail.com>
2023-02-03 16:11:38 +0200

    test: checking if env vars are loaded

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import app from "./../src/app";
 
- describe("Simple test case", () => {
+describe("Simple test case", () => {
   it("Add two numbers", () => {
     expect(2 + 2).toBe(4);
   });
@@ -10,11 +10,13 @@ import app from "./../src/app";
 describe("Route test", () => {
   it("get any specified route", async () => {
     const res = await request(app).get("/");
+    console.log(process.env);
+
     expect(res.status).toEqual(200);
-    expect(res.body).toEqual({ 
-    status: 200,
-    success: true,
-    message: `Welcome to team Axel's API! Endpoints available at http://localhost:8080/api/v1 + whatever endpoint you want to hit`,
+    expect(res.body).toEqual({
+      status: 200,
+      success: true,
+      message: `Welcome to team Axel's API! Endpoints available at http://localhost:8080/api/v1 + whatever endpoint you want to hit`,
     });
   });
 });
