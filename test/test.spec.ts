e888f77047c548ff8c6730247045451272fd6048
hakifred <hakifred20@gmail.com>
2023-02-06 11:06:25 +0300

    auth file

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -7,14 +7,14 @@ import app from "./../src/app";
   });
 });
 
-describe("Route test", () => {
-  it("get any specified route", async () => {
-    const res = await request(app).get("/");
-    expect(res.status).toEqual(200);
-    expect(res.body).toEqual({ 
-    status: 200,
-    success: true,
-    message: `Welcome to team Axel's API! Endpoints available at http://localhost:8080/api/v1 + whatever endpoint you want to hit`,
-    });
-  });
-});
+// describe("Route test", () => {
+//   it("get any specified route", async () => {
+//     const res = await request(app).get("/");
+//     expect(res.status).toEqual(200);
+//     expect(res.body).toEqual({ 
+//     status: 200,
+//     success: true,
+//     message: `Welcome to team Axel's API! Endpoints available at http://localhost:8080/api/v1 + whatever endpoint you want to hit`,
+//     });
+//   });
+// });
