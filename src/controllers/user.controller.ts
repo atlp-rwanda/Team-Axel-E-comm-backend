a8650c47d25186fa148eb3de8e156713b46ca303
SergeInGithub <nyiriserge@gmail.com>
2023-02-07 22:21:38 +0200

    ft: add password reset via email

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,9 +7,13 @@ import {
   findRegisteredUserService,
   confirmUserService,
   sendEmailConfirmationMessage,
+  sendResetRequestEmail,
+  sendPasswordResetConfirmation,
 } from '../services/_index';
 import { User } from '../db/schemas/_index';
 import { JwtUtility } from '../utils/_index';
+import { json } from 'sequelize';
+import { send } from 'process';
 
 // Get all users
 export const getAllUsers = async (req: Request, res: Response) => {
@@ -134,3 +138,133 @@ export const confirmUser = async (req: Request, res: Response) => {
     }
   }
 };
+// Request Password Reset
+export const resetPasswordRequestController = async (
+  req: Request,
+  res: Response
+) => {
+  // const requestPasswordResetService = await requestPasswordResetService(
+  //   req.body.email
+  // );
+  // return res.json(requestPasswordResetService);
+  const user = await User.findOne({ where: { email: req.body.email } });
+  if (!user) {
+    res
+      .status(404)
+      .json({ status: 404, success: false, message: 'Email does not exist' });
+  } else {
+    const payload = {
+      email: user.dataValues.email,
+    };
+    const resetToken = JwtUtility.generateToken(payload);
+    const updateResetToken = await User.update(
+      { resetToken: resetToken },
+      { where: { email: req.body.email } }
+    );
+    if (updateResetToken[0] === 1) {
+      res.status(200).json({
+        status: 200,
+        success: true,
+        message: `Please check your email for the link to reset your password `,
+      });
+      sendResetRequestEmail(
+        user.dataValues.email,
+        user.dataValues.surName,
+        resetToken
+      );
+    } else {
+      res.status(505).json({
+        status: 505,
+        success: false,
+        message: `Failed to update Reset token`,
+      });
+    }
+  }
+};
+// Reset Password
+export const resetPasswordController = async (req: Request, res: Response) => {
+  const resetToken = req.params.token;
+  const currentUser = await User.findOne({
+    where: { resetToken },
+  });
+
+  if (!currentUser) {
+    res.status(404).json({ status: 404, success: false, message: 'Forbidden' });
+  } else {
+    try {
+      const decodedToken = await JwtUtility.verifyToken(resetToken);
+
+      const updatedResponse = await User.update(
+        { password: req.body.password },
+        { where: { email: decodedToken.email } }
+      );
+      if (updatedResponse[0] === 1) {
+        res.status(201).json({
+          status: 201,
+          success: true,
+          message: 'Password reset successfully',
+        });
+        sendPasswordResetConfirmation(
+          decodedToken.email,
+          currentUser.dataValues.surName
+        );
+      } else {
+        res.status(505).json({
+          status: 505,
+          success: false,
+          message: 'Failed to reset password',
+        });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        res.json({
+          success: false,
+          message: `${error.message}`,
+        });
+      } else {
+        console.log('Something went wrong', error);
+      }
+    }
+  }
+  // const resetToken = JwtUtility.verifyToken(req.params.token)
+};
+
+// // Reset Password
+// export const resetPassword = async (userId, token, password) => {
+//   let passwordResetToken = await Token.findOne({ userId });
+
+//   if (!passwordResetToken) {
+//     throw new Error('Invalid or expired password reset token');
+//   }
+
+//   console.log(passwordResetToken.token, token);
+
+//   const isValid = await bcrypt.compare(token, passwordResetToken.token);
+
+//   if (!isValid) {
+//     throw new Error('Invalid or expired password reset token');
+//   }
+
+//   const hash = await bcrypt.hash(password, Number(bcryptSalt));
+
+//   await User.updateOne(
+//     { _id: userId },
+//     { $set: { password: hash } },
+//     { new: true },
+//   );
+
+//   const user = await User.findById({ _id: userId });
+
+//   sendEmail(
+//     user.email,
+//     'Password Reset Successfully',
+//     {
+//       name: user.name,
+//     },
+//     './template/resetPassword.handlebars',
+//   );
+
+//   await passwordResetToken.deleteOne();
+
+//   return { message: 'Password reset was successful' };
+// };
