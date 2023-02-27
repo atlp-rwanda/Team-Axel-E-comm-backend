3981937704a9829db2da7f29edebc0a4bbe88b3a
hakifred <hakifred20@gmail.com>
2023-02-27 16:41:53 +0300

    feat: stripe payment checkout

--- src/controllers/Checkout.controller.ts
+++ src/controllers/Checkout.controller.ts
@@ -6,17 +6,23 @@ const stripe = new Stripe(process.env.STRIPE_SECRET as string, {
 });
 
 export const checkoutController = {
-  async checkoutPayment(_req: Request, res: Response) {
+  async checkoutPayment(req: Request, res: Response) {
     try {
       // we still need ID coming from the front-end for this to work
+      const customer = await stripe.customers.create({
+        email: req.body.email,
+        source: req.body.tokenId,
+        description: 'Cypherteam customer',
+      });
+
       const paymentIntent = await stripe.paymentIntents.create({
         amount: 4000, // generic number
         currency: 'usd',
+        customer: customer.id,
+        confirm: true,
       });
 
-      res.send({
-        clientSecret: paymentIntent.client_secret,
-      });
+      res.status(200).json('successfully makde a payment');
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ success: false, error: err.message });
