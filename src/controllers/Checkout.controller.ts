a4f957beef80efd1c92636bd73bda0d00ed0d411
hakifred <hakifred20@gmail.com>
2023-03-04 13:05:06 +0300

    feat: added payment feature

--- src/controllers/Checkout.controller.ts
+++ src/controllers/Checkout.controller.ts
@@ -1,32 +1,42 @@
-import { Request, Response } from 'express';
-import Stripe from 'stripe';
+import { Request, Response } from "express";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET as string, {
-  apiVersion: '2022-11-15',
+  apiVersion: "2022-11-15",
 });
 
 // This controller will record a customer and also make charges
-// for now it's 4000 is generic number
 export const checkoutController = {
   async checkoutPayment(req: Request, res: Response) {
     try {
       const customer = await stripe.customers.create({
         email: req.body.email,
         source: req.body.tokenId,
-        description: 'Cypherteam customer',
+        description: "Cypherteam customer",
       });
 
       await stripe.paymentIntents.create({
         amount: 4000, // generic number
-        currency: 'usd',
+        currency: "usd",
         customer: customer.id,
         confirm: true,
       });
 
-      res.status(200).json('successfully ma a payment');
+      return res.status(200).json({
+        message: "Successfully made payment",
+        succes: true,
+        status: 200,
+      });
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ success: false, error: err.message });
+        return res
+          .status(400)
+          .json({
+            success: false,
+            error: err.message,
+            message: "Failed to make payment",
+            status: 400,
+          });
       }
     }
   },
