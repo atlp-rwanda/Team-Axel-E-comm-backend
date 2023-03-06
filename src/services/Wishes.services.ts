eb171bedbe78aeda0396ecbdc2a9e0de0116e46e
IYADUKUNZE Emile <iyaemile4@gmail.com>
2023-03-06 21:41:23 +0200

    feat: buyer can add product to wishlist(post,get and delete)

---
+++ src/services/Wishes.services.ts
@@ -0,0 +1,77 @@
+import Product from "../database/models/Product.model";
+import Wishlist from "../database/models/Wishes.model";
+import { WishListIteminterface } from "../interfaces";
+
+export const addToWishlist = async (wishlistItem: WishListIteminterface) => {
+  const { userId, productId } = wishlistItem;
+
+  const product = await Product.findOne({ where: { id: productId } });
+  if (!product) {
+    throw new Error(`🍎 Product with id ${productId} not found`);
+  }
+  const isProductInWishlist = await Wishlist.findOne({
+    where: { userId: userId, productId: productId },
+  });
+  if (isProductInWishlist) {
+    throw new Error(
+      `🍎 Product with id ${productId} is already in the wishlist`,
+    );
+  } else {
+    const wishes = await Wishlist.create({ userId, productId });
+    return wishes;
+  }
+};
+
+export const getAllWishes = async (userId: string) => {
+  const Allwishes = await Wishlist.findAll({
+    where: {
+      userId,
+    },
+  });
+
+  const productIds = Allwishes.map((wish) => wish.dataValues.productId);
+  const products = await Product.findAll({
+    where: {
+      id: productIds,
+    },
+  });
+
+  const result = products.map((product) => {
+    return {
+      name: product.dataValues.name,
+      price: product.dataValues.price,
+      image: product.dataValues.images,
+    };
+  });
+
+  return result;
+};
+
+export const clearWish = async (userId: string) => {
+  const clearwishRequest = await Wishlist.destroy({
+    where: {
+      userId,
+    },
+  });
+
+  return clearwishRequest;
+};
+
+export const clearOneWish = async (productId: string) => {
+  const available = await Wishlist.findOne({
+    where: {
+      productId,
+    },
+  });
+
+  if (!available) {
+    throw new Error(`🍎 Product with id ${productId} not found in wishlist`);
+  } else {
+    const clearwishRequest = await Wishlist.destroy({
+      where: {
+        productId,
+      },
+    });
+    return clearwishRequest;
+  }
+};
