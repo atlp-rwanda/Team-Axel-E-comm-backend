e50e64b09f4434e43124fb3bb15904baf70ca5dc
SergeInGithub <nyiriserge@gmail.com>
2023-03-13 19:09:47 +0200

    ft(notification): read all/a notification by ID [#184361235]

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import cartRouter from "./cart.routes";
 import checkoutRouter from "./checkout.routes";
 import orderRouter from "./order.routes";
 import wishRouter from "./wishes.routes";
+import notificationRouter from "./notification.routes";
 
 const router = Router();
 
@@ -18,5 +19,6 @@ router.use("/user", userRouter);
 router.use("/checkout", checkoutRouter);
 router.use("/order", orderRouter);
 router.use("/wishes", wishRouter);
+router.use("/notification", notificationRouter);
 
 export default router;
